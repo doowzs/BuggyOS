e09c21d3e4cba4307c02aa3d20c756755112d015
171860508-Tianyun Zhang <doowzs@qq.com>
2018-12-07 15:18:05 +0800

    [ADD] instruction memory and sample program

---
+++ cpu/instr_memory/instr_memory.v
@@ -0,0 +1,18 @@
+module instr_memory(
+  input   [31:0] addr,
+  output  [31:0] instr
+)
+
+  wire [7:0] paddr;
+  assign paddr = addr[9:2];
+
+  // SIZE: 256 instructions.
+  reg [31:0] instr_memory [255:0];
+  
+  initial begin
+    $readmemh("program/dummy.mips", instr_memory);
+  end
+
+  assign instr = instr_memory[paddr];
+
+endmodule
