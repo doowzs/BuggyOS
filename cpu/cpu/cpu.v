df3f7e177108389e6c4bdf1a5129774f9c42e20d
Tianyun Zhang <doowzs@qq.com>
2018-12-08 13:46:42 +0800

    control unit (cpu)

--- cpu/cpu/cpu.v
+++ cpu/cpu/cpu.v
@@ -52,6 +52,11 @@ module cpu(
     .instr(instr)
   );
 
+  sign_ex mSIEX(
+    .in(instr[15:0]),
+    .out(instr_sign_ex)
+  );
+
   decoder mDECODER(
     .instr(instr),
     .alu_zero(alu_eflags_zf),
@@ -64,11 +69,6 @@ module cpu(
     .pc_control(signal_pc_control)
   );
 
-  sign_ex mSIEX(
-    .in(instr[15:0]),
-    .out(instr_sign_ex)
-  );
-
   assign reg_raddr0 = instr[25:21];
   assign reg_raddr1 = instr[20:16];
 
@@ -77,6 +77,48 @@ module cpu(
     .in1(instr[15:11]),
     .sel(signal_reg_file_rmux_sel),
     .out(reg_waddr),
+  );
+  
+  mux21 mALUMUX(
+    .in0(reg_rdata1),
+	 .in1(instr_sign_ex),
+	 .sel(signal_alu_mux_sel),
+	 .out(alu_src)
+  );
+  
+  mux21 mMEMMUX(
+    .in0(data_mem_rdata),
+	 .in1(alu_result),
+	 .sel(signal_reg_file_dmux_sel),
+	 .out(reg_wdata)
+  );
+  
+  register_file mREG(
+    .clk(clk),
+	 .raddr0(reg_raddr0),
+    .rdata0(reg_rdata0),
+    .raddr1(reg_raddr1),
+    .rdata1(reg_rdata1),
+    .waddr(reg_waddr),
+    .wdata(reg_wdata),
+    .wren(signal_reg_file_wren)
+  );
+  
+  alu mALU(
+    .op(signal_alu_op),
+    .rs(reg_rdata0),
+    .rt(alu_src),
+    .rd(alu_dest),
+    .zf(alu_eflags_zf),
+    .of(alu_eflags_of)
+  );
+  
+  data_memory mMEM(
+	 .address(alu_dest), // Rs + offset
+	 .clock(clk),
+	 .data(reg_rdata1),  // Rt
+	 .wren(signal_data_mem_wren),
+	 .q(data_mem_rdata)
   );
 
 endmodule
