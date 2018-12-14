c2a5e112b735fcf2a20e4660f23574467c0c8464
Tianyun Zhang <doowzs@qq.com>
2018-12-15 01:45:41 +0800

    software: avoid duplicate characters
    save last input and avoid duplicate input

--- program/keyboard.asm
+++ program/keyboard.asm
@@ -8,15 +8,31 @@ addi $s0, $s0, 0x0000000d # ENTER KEY
 j _read
 
 _read:
-lw $a0, 0x100020D0
-bne $a0, $zero, _write
-j _read
+lw $a0, 0x100020D0 # read this key
+lw $t0, 0x100020D4 # load last key
+sw $a0, 0x100020D4 # save this key
+beq $a0, $zero, _read  # key != 0
+beq $a0, $t0, _read    # key != last
+# if =0x8, goto backspace
+# otherwise, print the character
+add $t0, $zero, 0x8
+beq $a0, $t0, _backspace
+j _write
 
 _write:
 sw $a0, ($k0)
 addi $k0, $k0, 0x4
 beq $a0, $s0, _newline
 beq $k0, $k1, _newline
+j _read
+
+_backspace:
+subi $k1, $k1, 0x118
+beq $k0, $k1, _backspace_ret
+subi $k0, $k0, 0x4
+sw $zero, ($k0)
+_backspace_ret:
+addi $k1, $k1, 0x118
 j _read
 
 _newline:
