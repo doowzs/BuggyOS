81c3e62102795d2fd21f89a19a1db69f7568398b
Maxewll-Lyu <xlworkshop.chs@hotmail.com>
2018-12-17 22:33:10 +0800

    Implement: LED (UNTESTED, full function

--- program/led.asm
+++ program/led.asm
@@ -1,12 +1,40 @@
-addi $s7, $zero, 0xfff
-addi $s7, $zero, 0x0
-addi $s7, $zero, 0x001
-addi $s7, $zero, 0x002
-addi $s7, $zero, 0x004
-addi $s7, $zero, 0x008
-addi $s7, $zero, 0x010
-addi $s7, $zero, 0x020
-addi $s7, $zero, 0x040
-addi $s7, $zero, 0x080
-addi $s7, $zero, 0x100
-addi $s7, $zero, 0x200
+ledctr:
+sw $ra, ($sp) 
+subi $sp, $sp, 0x4
+add $t0, $zero, $a0
+addi $t1, $zero, 0x1
+_ledctr_loop:
+beq $t0, $zero, _ledctr_handle
+sll $t1, $t1, 0x1
+subi $t0, $t0, 0x1
+j _ledctr_loop
+_ledctr_handle:
+addi $a0, $zero, 0x1000D000
+addi $a0, $a0, 0x18
+addi $a1, $zero, 0x10002A40
+sw $a0, ($sp) 
+subi $sp, $sp, 0x4
+jal strcmp
+addi $sp, $sp, 0x4
+lw $a0, ($sp)
+bne $v0, $zero, _ledctr_off
+addi $a1, $zero, 0x10002A30
+sw $a0, ($sp) 
+subi $sp, $sp, 0x4
+jal strcmp
+addi $sp, $sp, 0x4
+lw $a0, ($sp)
+bne $v0, $zero, _ledctr_on
+xor $s7, $s7, $t1# default
+j _ledctr_ret
+_ledctr_off:
+nor $t1, $t1, $t1
+and $s7, $s7, $t1
+j _ledctr_ret
+_ledctr_on:
+or $s7, $s7, $t1
+_ledctr_ret:
+addi $sp, $sp, 0x4
+lw $ra, ($sp)
+jr $ra
+
--- program/system.asm
+++ program/system.asm
@@ -141,7 +141,8 @@ _cmd_04_led:
 addi $a0, $zero, 0x1000D000
 addi $a0, $a0, 0x10
 jal scanhex
-add $s7, $zero, $v0
+add $a0, $zero, $v0
+jal ledctr
 j _handler_ret
 _cmd_restart:
 j _init
@@ -203,7 +204,6 @@ _cmpn_fin:
 addi $sp, $sp, 0x4
 lw $ra, ($sp)
 jr $ra
-
 
 scanhex:
 sw $ra, ($sp) 
@@ -269,6 +269,46 @@ addi $t3, $t3, 0x30
 sw $t3, ($gp)
 addi $t4, $zero, 0x1000D000
 bne $gp, $t4, _printhex_loop
+addi $sp, $sp, 0x4
+lw $ra, ($sp)
+jr $ra
+
+ledctr:
+sw $ra, ($sp) 
+subi $sp, $sp, 0x4
+add $t0, $zero, $a0
+addi $t1, $zero, 0x1
+_ledctr_loop:
+beq $t0, $zero, _ledctr_handle
+sll $t1, $t1, 0x1
+subi $t0, $t0, 0x1
+j _ledctr_loop
+_ledctr_handle:
+addi $a0, $zero, 0x1000D000
+addi $a0, $a0, 0x18
+addi $a1, $zero, 0x10002A40
+sw $a0, ($sp) 
+subi $sp, $sp, 0x4
+jal strcmp
+addi $sp, $sp, 0x4
+lw $a0, ($sp)
+bne $v0, $zero, _ledctr_off
+addi $a1, $zero, 0x10002A30
+sw $a0, ($sp) 
+subi $sp, $sp, 0x4
+jal strcmp
+addi $sp, $sp, 0x4
+lw $a0, ($sp)
+bne $v0, $zero, _ledctr_on
+xor $s7, $s7, $t1# default
+j _ledctr_ret
+_ledctr_off:
+nor $t1, $t1, $t1
+and $s7, $s7, $t1
+j _ledctr_ret
+_ledctr_on:
+or $s7, $s7, $t1
+_ledctr_ret:
 addi $sp, $sp, 0x4
 lw $ra, ($sp)
 jr $ra
